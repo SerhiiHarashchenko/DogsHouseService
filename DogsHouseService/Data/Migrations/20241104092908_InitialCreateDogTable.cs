0255fd1d135b33130407b988c496c09d430fde35
Serhii Harashchenko <harashchenkoserhii@gmail.com>
2024-11-04 11:32:03 +0200

    add initial migration

---
+++ DogsHouseService/Data/Migrations/20241104092908_InitialCreateDogTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogsHouseService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateDogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "dog_table",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    TailLength = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dog_table", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dog_table_Name",
+                table: "dog_table",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dog_table");
+        }
+    }
+}
